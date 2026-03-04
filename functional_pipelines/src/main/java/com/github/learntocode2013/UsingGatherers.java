2c284fbc346fed2dda92d892e94db7985f375908
Dibakar Sen <learntocode2013@yahoo.com>
2026-03-04 07:42:52 +0530

    PRO-6: Implement demo code for sequential stateful gatherer

--- functional_pipelines/src/main/java/com/github/learntocode2013/UsingGatherers.java
+++ functional_pipelines/src/main/java/com/github/learntocode2013/UsingGatherers.java
@@ -1,7 +1,10 @@
 package com.github.learntocode2013;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Gatherer;
+import lombok.NonNull;
 
 public class UsingGatherers {
   public static <T> boolean consumeAndPush(
@@ -17,5 +20,25 @@ public class UsingGatherers {
       Consumer<T> consumer) {
    return Gatherer.ofSequential((_, element, downstream) ->
      consumeAndPush(element, consumer, downstream));
+  }
+
+  public record ValueWithIndex<E>(E value, int index) {
+    @NonNull
+    @Override
+    public String toString() {
+      return "%d: %s".formatted(index, value);
+    }
+  }
+
+  public static <T, R> Gatherer<? super T, AtomicInteger, ValueWithIndex<R>>
+  mapWithIndex(Function<T, R> mapper) {
+    return Gatherer.ofSequential(
+        AtomicInteger::new,
+        (index, element, downstream) ->
+            downstream.push(new ValueWithIndex<R>(
+                mapper.apply(element),
+                index.getAndIncrement()
+            ))
+        );
   }
 }
