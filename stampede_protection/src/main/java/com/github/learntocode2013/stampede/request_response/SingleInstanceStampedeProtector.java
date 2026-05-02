1c8ca223364dea047375f732a471780484898a6d
Dibakar Sen <learntocode2013@yahoo.com>
2026-05-02 19:38:44 +0530

    Fix some code smell in single instance stampede implementation

--- stampede_protection/src/main/java/com/github/learntocode2013/stampede/request_response/SingleInstanceStampedeProtector.java
+++ stampede_protection/src/main/java/com/github/learntocode2013/stampede/request_response/SingleInstanceStampedeProtector.java
@@ -10,8 +10,15 @@ import java.util.logging.Logger;
 
 public class SingleInstanceStampedeProtector implements StampedeProtector {
     private static final Logger log = Logger.getLogger(SingleInstanceStampedeProtector.class.getName());
-    private static final ConcurrentHashMap<String, CompletableFuture<DomainResponse>> inFlightRequests =
+    private final ConcurrentHashMap<String, CompletableFuture<DomainResponse>> inFlightRequests =
             new ConcurrentHashMap<>();
+    private static final StampedeProtector INSTANCE = new SingleInstanceStampedeProtector();
+
+    private SingleInstanceStampedeProtector() {}
+
+    public static StampedeProtector getInstance() {
+        return INSTANCE;
+    }
 
     @Override
     public CompletableFuture<DomainResponse> fetchWithProtection(
@@ -26,29 +33,23 @@ public class SingleInstanceStampedeProtector implements StampedeProtector {
         }
         log.log(Level.INFO, "Thread: {0} observed a cache miss for {1}",
                 new Object[]{Thread.currentThread().getName(), cacheKey});
-        var resultCF = inFlightRequests.computeIfAbsent(cacheKey, k -> {
-           CompletableFuture<DomainResponse> newRequestCF = new CompletableFuture<>();
+        var result = inFlightRequests.computeIfAbsent(cacheKey, k -> {
            var value = valueSupplierFromCache.get();
            if (Objects.nonNull(value)) {
                log.log(Level.INFO, "Thread: {0} now found a value for cache key: {1} in second try",
                         new Object[]{Thread.currentThread().getName(), cacheKey});
                return CompletableFuture.completedFuture(value);
             }
-           CompletableFuture.supplyAsync(valueSupplierOnCacheMiss)
-                   .whenComplete((domainResponse, error) -> {
-                       if (Objects.nonNull(error)) {
-                           log.log(Level.WARNING, "Failed to hydrate cache for key: {0}", cacheKey);
-                           newRequestCF.completeExceptionally(error);
-                       } else {
-                           log.log(Level.INFO, "{0} - Cache was hydrated with value: {1} for key: {2}",
-                                   new Object[]{Thread.currentThread().getName(), domainResponse, cacheKey});
-                           newRequestCF.complete(domainResponse);
-                       }
-                   });
+
+           CompletableFuture<DomainResponse> newRequestCF = CompletableFuture.supplyAsync(valueSupplierOnCacheMiss);
+           newRequestCF.whenComplete((response, error) -> inFlightRequests.remove(cacheKey, newRequestCF));
            return newRequestCF;
         });
-        resultCF.whenComplete((domainResponse, error) -> inFlightRequests.remove(cacheKey));
-        return resultCF;
+
+        if (result.isDone()) {
+            inFlightRequests.remove(cacheKey, result);
+        }
+        return result;
     }
 
     @Override
--- stampede_protection/src/test/java/com/github/learntocode2013/stampede/request_response/SingleInstanceStampedeProtectorTest.java
+++ stampede_protection/src/test/java/com/github/learntocode2013/stampede/request_response/SingleInstanceStampedeProtectorTest.java
@@ -16,20 +16,15 @@ import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SingleInstanceStampedeProtectorTest {
     private static final Logger log = Logger.getLogger(SingleInstanceStampedeProtectorTest.class.getName());
     private static final String CACHE_KEY = "catalog-state-2026-05-01";
     private static final ConcurrentHashMap<String, DomainResponse> RESPONSE_CACHE = new ConcurrentHashMap<>();
-    private static final StampedeProtector protector = new SingleInstanceStampedeProtector();
+    private static final StampedeProtector protector = SingleInstanceStampedeProtector.getInstance();
     private static final Supplier<DomainResponse> valueSupplierFromCache = () -> RESPONSE_CACHE.get("catalog-state-2026-05-01");
     private static final Supplier<DomainResponse> valueSupplierOnCacheMiss = () -> fetchFromCatalogService("2026-05-01", CACHE_KEY)
             .getOrNull();
     private static final AtomicInteger DEPENDENCY_INVOCATION_COUNTER = new AtomicInteger(0);
-
-    @BeforeAll
-    static void setUp() {
-    }
 
     @AfterAll
     static void tearDown() {
@@ -40,10 +35,10 @@ class SingleInstanceStampedeProtectorTest {
     @AfterEach
     void afterEach() {
         DEPENDENCY_INVOCATION_COUNTER.set(0);
+        RESPONSE_CACHE.clear();
     }
 
     @Test
-    @Order(1)
     @SneakyThrows
     void verifyRequestCoalescingOnCacheMiss() {
         Assertions.assertEquals(0, DEPENDENCY_INVOCATION_COUNTER.get());
@@ -71,12 +66,13 @@ class SingleInstanceStampedeProtectorTest {
     }
 
     @Test
-    @Order(2)
     @SneakyThrows
     void verifyNoInflightRequests_OnCacheHit() {
+        RESPONSE_CACHE.put(CACHE_KEY, new DomainResponse("CATALOG_FULL", "2026-05-01"));
         Assertions.assertEquals(0, DEPENDENCY_INVOCATION_COUNTER.get());
         var parallelRequests = Runtime.getRuntime().availableProcessors();
         List<CompletableFuture<DomainResponse>> futures = Collections.synchronizedList(new ArrayList<>());
+        CountDownLatch latch = new CountDownLatch(parallelRequests);
         try(ExecutorService executorService = Executors.newFixedThreadPool(parallelRequests)) {
             for(int i = 0; i < parallelRequests; i++) {
                 executorService.execute(() -> {
@@ -86,9 +82,11 @@ class SingleInstanceStampedeProtectorTest {
                             valueSupplierOnCacheMiss
                     );
                     futures.add(resultCF);
+                    latch.countDown();
                 });
 
             }
+            latch.await();
             CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
             Assertions.assertTrue(protector.getInFlightRequestForKey(CACHE_KEY).isEmpty());
         } catch (Exception ex) {
