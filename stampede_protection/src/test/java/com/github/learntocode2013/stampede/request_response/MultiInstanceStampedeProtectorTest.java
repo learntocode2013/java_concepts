48c74c82bd04daacf8f72cfc9435ae0cf9933b9e
Dibakar Sen <learntocode2013@yahoo.com>
2026-05-04 15:51:26 +0530

    Disable the tests so that build passes in the absence of docker environment

--- stampede_protection/src/test/java/com/github/learntocode2013/stampede/request_response/MultiInstanceStampedeProtectorTest.java
+++ stampede_protection/src/test/java/com/github/learntocode2013/stampede/request_response/MultiInstanceStampedeProtectorTest.java
@@ -23,9 +23,11 @@ import java.util.logging.Logger;
 import static com.github.learntocode2013.stampede.request_response.MultiInstanceStampedeProtector.JOB_STATE_MAP;
 
 // ----------------------------------------------------------------
-// NOTE: Execute the docker command below before running this test.
+// NOTE: Execute the docker command below before uncommenting the
+// Disabled tag to run the tests.
 // docker run -d --name redis-local -p 6379:6379 redis:7.2
 // ---------------------------------------------------------------
+@Disabled
 class MultiInstanceStampedeProtectorTest {
     private static final Logger log = Logger.getLogger(MultiInstanceStampedeProtectorTest.class.getName());
     private static final AtomicInteger DEPENDENCY_INVOCATION_COUNTER = new AtomicInteger(0);
